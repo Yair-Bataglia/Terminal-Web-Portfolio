8a33f6f947505295a35d5ae07f121d9cf0728c8f
Yair-Bataglia <batagliayair@gmail.com>
2025-01-19 20:12:09 -0300

    Repare la estructura, agregue el dist y todo el codigo fuente en SRC

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: './src',  // Si tu archivo index.html está en la carpeta src
+  build: {
+    outDir: '../dist', // Establece la carpeta de salida del build
+  },
+});
