8377481a50952249b5df7f0c4aa6621c62f454e7
Yair-Bataglia <batagliayair@gmail.com>
2025-01-20 22:25:51 -0300

    Modifico el vite.config para generar bien el dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: './src',  // Si tu archivo index.html está en la carpeta src
   build: {
+
     outDir: '../dist', // Establece la carpeta de salida del build
   },
+  base:"/portfolio/"
 });
 
